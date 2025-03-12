954dde67e939bff9540d4e80eba02ecc675fb71f
Adler Almeida <adler-almeida@hotmail.com>
2025-03-11 22:58:42 -0300

    mudança no espaçamento

--- Curso-em-video/Desafio-09.py
+++ Curso-em-video/Desafio-09.py
@@ -2,7 +2,7 @@
 
 print ('======== DESAFIO 009 ========')
 print ()
-print ('Um programa que leia um \nnúmero inteiro qualquer e \nmostre na tela a sua tabuada.')
+print ('Crie programa que leia um \nnúmero inteiro qualquer e \nmostre na tela a sua tabuada.')
 print ()
 print ('=============================')
 print ()
@@ -21,15 +21,15 @@ n8 = valor * 8
 n9 = valor * 9
 n10 = valor * 10
 
-print ('{} x 1 = {}'.format(valor,n1))
-print ('{} x 2 = {}'.format(valor,n2))
-print ('{} x 3 = {}'.format(valor,n3))
-print ('{} x 4 = {}'.format(valor,n4))
-print ('{} x 5 = {}'.format(valor,n5))
-print ('{} x 6 = {}'.format(valor,n6))
-print ('{} x 7 = {}'.format(valor,n7))
-print ('{} x 8 = {}'.format(valor,n8))
-print ('{} x 9 = {}'.format(valor,n9))
+print ('{} x  1 = {}'.format(valor,n1))
+print ('{} x  2 = {}'.format(valor,n2))
+print ('{} x  3 = {}'.format(valor,n3))
+print ('{} x  4 = {}'.format(valor,n4))
+print ('{} x  5 = {}'.format(valor,n5))
+print ('{} x  6 = {}'.format(valor,n6))
+print ('{} x  7 = {}'.format(valor,n7))
+print ('{} x  8 = {}'.format(valor,n8))
+print ('{} x  9 = {}'.format(valor,n9))
 print ('{} x 10 = {}'.format(valor,n10))
 
 print ()
