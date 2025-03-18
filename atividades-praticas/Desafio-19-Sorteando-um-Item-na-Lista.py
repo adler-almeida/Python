cea15418bd9d9e22633d837349953b9bf8935f08
Adler Almeida <adler-almeida@hotmail.com>
2025-03-18 19:05:31 -0300

    Initial commit

--- atividades-praticas/Desafio-19-Sorteando-um-Item-na-Lista.py
+++ atividades-praticas/Desafio-19-Sorteando-um-Item-na-Lista.py
@@ -17,8 +17,17 @@ aluno3 = str(input('Favor informar o nome do 3º aluno: '))
 aluno4 = str(input('Favor informar o nome do 4º aluno: '))
 print ()
 
-#agora realizar o sorteio aleatorio de uma str.
-sorteado = random.choice([aluno1,aluno2,aluno3,aluno4])
-print ('O Aluno sorteado para realizar a tarefa de apagar o quadro será {}.'.format(sorteado))
+#criando a lista de alunos
+alunos = [aluno1,aluno2,aluno3,aluno4]
+
+#escolhendo um aluno aleatório
+sorteado = random.choice(alunos)
+
+#exibir na tela
+print ('O Aluno sorteado para realizar a tarefa \nde apagar o quadro será {}.'.format(sorteado))
 print ()
 print ('='*43)
+
+"""
+Se quiser um único nome aleatório, use random.choice(). Se quiser misturar e exibir todos na nova ordem, use random.shuffle().
+"""
--- atividades-praticas/Desafio-20-Sorteando-uma-Ordem-na-Lista.py
+++ atividades-praticas/Desafio-20-Sorteando-uma-Ordem-na-Lista.py
@@ -1,4 +1,33 @@
-'''
+"""
 O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos.
 Faça um programa que leia o nome dos quatro alunos e mostre a ordem sorteada.
-'''
+"""
+
+import random
+
+print ()
+print ('='*43)
+print ('                  DESAFIO 20')
+print ('='*43)
+print ()
+
+#receber nome de alunos
+aluno1 = str(input('Favor informar o nome do 1º aluno: '))
+aluno2 = str(input('Favor informar o nome do 2º aluno: '))
+aluno3 = str(input('Favor informar o nome do 3º aluno: '))
+aluno4 = str(input('Favor informar o nome do 4º aluno: '))
+print ()
+
+#agora criar uma lista com os alunos e embaralhar..
+alunos = [aluno1,aluno2,aluno3,aluno4]   #lista precisa ser declarada em []
+
+#embaralhar a lista
+random.shuffle(alunos)
+
+print ('A ordem sorteada dos Alunos são: ')
+print (alunos)
+print ('='*43)
+
+"""
+Se quiser um único nome aleatório, use random.choice(). Se quiser misturar e exibir todos na nova ordem, use random.shuffle().
+"""
