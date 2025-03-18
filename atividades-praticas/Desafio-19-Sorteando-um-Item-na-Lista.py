b846222a678f707f5dbaa5885db4556eb08a5bf8
Adler Almeida <adler-almeida@hotmail.com>
2025-03-18 18:30:36 -0300

    Initial commit

--- atividades-praticas/Desafio-19-Sorteando-um-Item-na-Lista.py
+++ atividades-praticas/Desafio-19-Sorteando-um-Item-na-Lista.py
@@ -10,7 +10,15 @@ print ('                  DESAFIO 19')
 print ('='*43)
 print ()
 
+#receber nome de alunos
 aluno1 = str(input('Favor informar o nome do 1º aluno: '))
 aluno2 = str(input('Favor informar o nome do 2º aluno: '))
 aluno3 = str(input('Favor informar o nome do 3º aluno: '))
 aluno4 = str(input('Favor informar o nome do 4º aluno: '))
+print ()
+
+#agora realizar o sorteio aleatorio de uma str.
+sorteado = random.choice([aluno1,aluno2,aluno3,aluno4])
+print ('O Aluno sorteado para realizar a tarefa de apagar o quadro será {}.'.format(sorteado))
+print ()
+print ('='*43)
