eb7d58cb76b61465591e9115bbaa19608073d0ac
Adler Almeida <adler-almeida@hotmail.com>
2025-03-18 16:11:11 -0300

    Initial commit

--- Curso-em-video/Desafio-17-Catetos-e-Hipotenusa.py
+++ Curso-em-video/Desafio-17-Catetos-e-Hipotenusa.py
@@ -1,2 +1,24 @@
-#Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retângulo,
-#calcule e mostre o comprimento da hipotenusa.
+'''
+Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retângulo,
+calcule e mostre o comprimento da hipotenusa.
+**Importando modulo.**
+'''
+
+#Pode ser feito também usando a formula de Teorema de Pitágoras = c² = a² + b², a e b são catetos, c é a hipotenusa.
+
+from math import hypot, ceil
+
+print ()
+print ('='*43)
+print ('                 DESAFIO 17')
+print ('='*43)
+print ()
+
+catetoa = float(input('Informe o Cateto A: '))
+catetob = float(input('Informe o Cateto B: '))
+hipotenusa = hypot(catetoa,catetob)
+
+print ()
+print ('A Hipotenusa mede {}.'.format(ceil(hipotenusa)))
+print ()
+print ('='*43)
