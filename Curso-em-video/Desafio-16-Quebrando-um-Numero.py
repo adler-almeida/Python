5dca210cfffa10df5dcc465354f0f7cd912a99e2
Adler Almeida <adler-almeida@hotmail.com>
2025-03-18 15:22:40 -0300

    Initial commit

--- Curso-em-video/Desafio-16-Quebrando-um-Numero.py
+++ Curso-em-video/Desafio-16-Quebrando-um-Numero.py
@@ -1,3 +1,22 @@
-#Crie um programa que leia um número qualquer pelo teclado, e mostre na tela, a sua porção inteira.
-#Ex: 6.127, parte inteira 6.
-#importando modulos.
+"""
+Crie um programa que leia um número qualquer pelo teclado, e mostre na tela, a sua porção inteira.
+Ex: 6.127, parte inteira 6.
+importando modulos.
+"""
+from math import trunc
+#Importei (de, matematica, importar, números antes da vírgula = form math import trunc
+
+print ()
+print ('='*43)
+print ('                 DESAFIO 16')
+print ('='*43)
+print ()
+
+ndecimal = float(input('Digite um numero decimal: '))
+ninteiro = trunc(ndecimal)
+
+print ()
+print ('Número informado: {}. \nParte inteira é: {}.'.format(ndecimal,ninteiro))
+print ()
+print ('='*43)
+
