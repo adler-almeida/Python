1da9f02b51e8072cb87147d421a536d5946f6a33
Adler Almeida <adler-almeida@hotmail.com>
2025-03-20 18:35:32 -0300

    Initial commit

--- atividades-praticas/Desafio-26-Primeira-e-ultima-ocorrencia-de-uma-string.py
+++ atividades-praticas/Desafio-26-Primeira-e-ultima-ocorrencia-de-uma-string.py
@@ -6,7 +6,19 @@ Faça um programa que leia uma frase pelo teclado e mostre:
 """
 
 print ()
-print ('='*43)
-print ('                  DESAFIO 26')
-print ('='*43)
+print ('='*53)
+print ('                    DESAFIO 26')
+print ('='*53)
 print ()
+
+# Salvando a frase e tornando maiuscula.
+frase = str(input('Escreva uma frase: '))
+frase = frase.upper()
+
+# Contando quantas vezes aparece a letra 'A'.
+print ()
+print ('A letra "a" aparece {} vezes.'.format(frase.count('A')))
+print ('A letra "a" aparece a primeira vez na posição {}'.format(frase.find('A')))
+print ('A letra "a" aparece a ultima vez na posição {}'.format(frase.rfind('A')))
+print ()
+print ('='*53)
